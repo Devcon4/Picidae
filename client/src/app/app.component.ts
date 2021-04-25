c3f0bbbcefcc9e4995d82aa98924f80362091060
Devyn Cyphers <devyncyphers@gmail.com>
2021-04-24 18:28:04 -0600

    change title

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Picidae version second branch!';
+  title = 'Picidae version new branch!';
 }
